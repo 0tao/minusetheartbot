1ca140c0b5e93d60cd5c2a54f77dcea2c770019a
Jack B. Du <jackbdu@nyu.edu>
2017-03-03 20:10:31 +0800

    kjhl (vim keys) added for controling the robot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-
 import sys     # exit
-import grovepi
+import grovepi # grovepi
 import grove_i2c_motor_driver
 import itg3200 # library for grove gyroscope
 import time    # sleep
@@ -12,41 +11,38 @@ gyro.default_init()
 # pin numbers for ultrasonic rangers # note D14 corresponds to A0
 ultrasonic_rangers = (8, 4, 3, 14, 2, 5, 7, 6)
 distances = [0] * 8
-# 0 denote clockwise, 1 denote counter-clockwise
-directions = [1, 0, 1, 1]
-speeds = [1, 100, 1, 100]
+# + denote clockwise, - denote counter-clockwise
+velocities = [1, 1, 1, 1]
 speedLimit = 100
 s = [100, 100]
 
 # initalize motors and return two motor pairs, 02 and 13
 def initMotors():
+    # motor driver addresses accordingly
     motors02 = grove_i2c_motor_driver.motor_driver(address=0x0f)
     motors13 = grove_i2c_motor_driver.motor_driver(address=0x0a)
     return motors02, motors13
 
-# set the directions of the motors
-def setDirections((motors02, motors13), directions):
-    directions02 = (directions[0]+1) * 4 + (directions[2]+1)
-    directions13 = (directions[1]+1) * 4 + (directions[3]+1)
+# set the velocities of the motors with the speedLimit as the fastest speed allowed
+def setVelocities((motors02, motors13), velocities, speedLimit):
+    # get directions from the velocities
+    directions02 = (1 if velocities[0] >= 0 else 2) * 4 + (1 if velocities[2] >= 0 else 2)
+    directions13 = (1 if velocities[1] >= 0 else 2) * 4 + (1 if velocities[3] >= 0 else 2)
+    limitSpeeds(velocities, speedLimit)
+    # set speeds
+    motors02.MotorSpeedSetAB(abs(velocities[2]), abs(velocities[0]))    #defines the speed of motor 0 and motor 2
+    motors13.MotorSpeedSetAB(abs(velocities[3]), abs(velocities[1]))    #defines the speed of motor 1 and motor 3
+    # set directions
     motors02.MotorDirectionSet(directions02)
     motors13.MotorDirectionSet(directions13)
 
-# set the speeds of the motors with the speedLimit as the fastest speed allowed
-def setSpeeds((motors02, motors13), speeds, speedLimit):
-    limitSpeeds(speeds, speedLimit)
-    motors02.MotorSpeedSetAB(speeds[2], speeds[0])    #defines the speed of motor 0 and motor 2
-    motors13.MotorSpeedSetAB(speeds[3], speeds[1])    #defines the speed of motor 1 and motor 3
-
 # change the fastest speed to speedLimit and scale other speeds proportionally
-def limitSpeeds(speeds, speedLimit):
+def limitSpeeds(velocities, speedLimit):
     speedLimit += 0.0
-    maxSpeed = max(speeds)
+    maxSpeed = max(abs(max(velocities)), abs(min(velocities)))
     if maxSpeed > 0:
         for i in range(4):
-            if speeds[i] == maxSpeed:
-                speeds[i] = speedLimit
-            else:
-                speeds[i] *= speedLimit/maxSpeed
+            velocities[i] *= speedLimit/maxSpeed
 
 # get the distances from each ultrasonic ranger
 def getDistances(ultrasonic_rangers, distances):
@@ -57,66 +53,11 @@ def getDistances(ultrasonic_rangers, distances):
 def getRotation():
     # storing the z-axis gyro value
     return gyro.read_data()[2]+40
-#
-# normalize the speed by setting the faster wheel to ms
-# and scale the other wheel speed accordingly
-def normalize_s(xx_s, ms):
-    # set ms to 100 if out of boundary
-    if (ms > 100 or ms < 0):
-        ms = 100.0
-    else:
-        ms += 0.0
-
-    if (xx_s[1] <= xx_s[0]):
-        xx_s[1] *= ms/xx_s[0]
-        xx_s[0] = ms
-    elif (xx_s[0] <= xx_s[1]):
-        xx_s[0] *= ms/xx_s[1]
-        xx_s[1] = ms
-    print xx_s
-
-def forward(mlr, lr_s, gz):
-    #FORWARD
-    print("Forward")
-    if gz < -10: #turning closewise
-        lr_s[0] *= 95/100.0
-        lr_s[1] *= 100/95.0
-        
-    elif gz > 10: #turning counter-clockwise
-        lr_s[0] *= 100/95.0
-        lr_s[1] *= 95/100.0
-    normalize_s(lr_s, 50)
-    mlr.MotorSpeedSetAB(lr_s[0], lr_s[1])    #defines the speed of motor 1 and motor 2;
-    mlr.MotorDirectionSet(0b0110)    #"0b1010" defines the output polarity, "10" means the M+ is "positive" while the M- is "negtive"
-
-def backward(mlr, lr_s, gz):
-    #BACKWARD
-    print("Backward")
-    if gz < -10: #turning closewise
-        lr_s[0] *= 100/95.0
-        lr_s[1] *= 95/100.0
-        
-    elif gz > 10: #turning counter-clockwise
-        lr_s[0] *= 95/100.0
-        lr_s[1] *= 100/95.0
-    normalize_s(lr_s, 50)
-    mlr.MotorSpeedSetAB(lr_s[0], lr_s[1])    #defines the speed of motor 1 and motor 2;
-    mlr.MotorDirectionSet(0b1001)    #0b0101    Rotating in the opposite direction
-
-def leftward(mfb, fb_s, gz):
-    #LEFTWARD
-    print("Leftward")
-    forward(mfb, fb_s, gz)
-
-def rightward(mfb, fb_s, gz):
-    #RIGHTWARD
-    print("RIGHTWARD")
-    backward(mfb, fb_s, gz)
-
+# stop motors
 def stop(motors):
     #STOP
     print("Stop")
-    setSpeeds(motors, [0,0,0,0], 0);
+    setVelocities(motors, [0,0,0,0], 0);
     time.sleep(1)
 
 try:
@@ -134,13 +75,8 @@ try:
             # Read distance value from Ultrasonic
             getDistances(ultrasonic_rangers, distances)
             print("distances", distances)
-
-            #forward(motors[1], s, rotation)
-
-            setDirections(motors, directions)
-            print(directions)
-            setSpeeds(motors, speeds, speedLimit)
-            print(speeds)
+            setVelocities(motors, velocities, speedLimit)
+            print(velocities)
 
         except TypeError:
             print ("TypeError")
