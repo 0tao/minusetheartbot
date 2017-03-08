6c579e6150e9b0ede9e81937c024582df2c7a700
Jack B. Du <jackbdu@nyu.edu>
2017-03-09 00:18:44 +0800

    grovepi.py modified to fix ultrasonic ranger for firmware v1.2.6

--- grovepi.py
+++ grovepi.py
@@ -251,7 +251,7 @@ def temp(pin, model = '1.0'):
 def ultrasonicRead(pin):
 	write_i2c_block(address, uRead_cmd + [pin, unused, unused])
 	time.sleep(.06)	#firmware has a time of 50ms so wait for more than that
-	read_i2c_byte(address)
+	#read_i2c_byte(address)
 	number = read_i2c_block(address)
 	return (number[1] * 256 + number[2])
 
