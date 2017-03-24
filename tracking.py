9628000b2ba8a4f31b9a975e55097a9f3d2bec1b
JackBDu <jd2804@nyu.edu>
2017-03-25 00:42:41 +0800

    MATH CORRECTED

--- tracking.py
+++ tracking.py
@@ -5,9 +5,28 @@ import time
 import numpy as np
 import imutils
 import cv2
-import math
+import math     # math.pi, math.sin, math.cos
+import argparse # argparse
 from socket import *
 from random import randint
+
+__author__      = 'Jack B. Du (Jiadong Du)'
+__email__       = 'jackbdu@nyu.edu'
+__copyright__   = 'Copyright 2017, Jack B. Du (Jiadong Du)'
+__license__     = 'Apache-2.0'
+__status__      = 'Development'
+
+parser = argparse.ArgumentParser(description="Minus E the Art Bot - Client")
+parser.add_argument('-p', '--preview', action='store_false', help="Toggle preview")
+parser.add_argument('-b', '--botless', action='store_true', help="Toggle botless")
+parser.add_argument('-d', '--debug', action='store_false', help="Toggle debug")
+parser.add_argument('-o', '--out', action='store_false', help="Toggle output")
+
+args = parser.parse_args()
+BOTLESS = args.botless
+PREVIEW = args.preview
+DEBUG   = args.debug
+OUT     = args.out
  
 # define the lower and upper boundaries of blue and red color of 
 # the balls/markers in the HSV color space
@@ -30,28 +49,52 @@ rx, ry, bx, by = 0, 0, 1, 1
 width = 800
 height = 450
 
-debug = True
-
 prevAlpha = 0
 
 # maximum error threshold in pixels
-threshold = 30
+threshold = 10
 
 # shift the video coordinate system
-sx = 250
-sy = 150
+sx = 265
+sy = 100
 # route specifies a series of coordinates that the robot will move to
+center = [(width/2, height/2)]
 route1 = [(0,0),(0,50),(0,100),(50,100),(100,100),(150,100),(200,100),(250,100),(300,100),(300,50),(300,100),(250,100),(200,100),(150,100),(100,100),(50,100)]
 route2 = [(0,0),(0,150),(300,150),(300,0)]
 route3 = [(0,0),(0,50)]
-center = [(width/2, height/2)]
-route = route2
+route4 = [(0,0),(0,50)]
+
+canvasw = 240
+canvash = 240
+topLeft = (0,0)
+topRight = (250,0)
+bottomLeft = (0,250)
+bottomRight = (250,250)
+route = [topLeft]
 
 # currP stores the index of current coordinate in route
 currP = [0]
 
 # start opencv video capture with video0
 camera = cv2.VideoCapture(0)
+
+# read reference image
+img = cv2.imread('shirky.png', cv2.IMREAD_GRAYSCALE)
+img = cv2.resize(img,(20, 20), interpolation = cv2.INTER_CUBIC)
+#cv2.imwrite( "converted.png", img); 
+route = []
+values = []
+
+for r in range(20):
+    for c in range(20):
+        if (r%2==0):
+            route.append((6+c*canvash/20,6+r*canvasw/20))
+            values.append(255-img[r,c])
+        else:
+            route.append((6+(19-c)*canvash/20,6+r*canvasw/20))
+            values.append(255-img[r,19-c])
+print route
+print values
 
 # getting the width and height of the video capture
 #width=int(camera.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
@@ -60,14 +103,16 @@ camera = cv2.VideoCapture(0)
 # initialize video output info
 # codec for video output, works fine on macOS
 fourcc = cv2.cv.CV_FOURCC('m','p','4','v')
-out = cv2.VideoWriter()
-out.open('output.mp4', fourcc, 20, (width,height))
+if OUT:
+    out = cv2.VideoWriter()
+    out.open('output.mp4', fourcc, 20, (width,height))
 
-# initialize tcp connection
-serverName = '10.209.11.115'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+if not BOTLESS:
+    # initialize tcp connection
+    serverName = '10.209.11.115'
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
 
 # go to point (dstx, dsty) on the video stream
 def goTo(rx, ry, bx, by, dstx, dsty, curr):
@@ -116,24 +161,12 @@ def goTo(rx, ry, bx, by, dstx, dsty, curr):
     # alpha is the rotation of  (red-to-blue vector / x axis in) robot coordinate system
     # relative to (x axis of) the video coordinate system, clockwise denotes positive
     # 0 <= alpha <= 2*pi
-    alpha = abs(math.atan(slope))
+    alpha = math.atan(slope)
     # adjusting alpha based on which quadrant the red-to-blue vector lies
-    if dx > 0:
-        if dy < 0:
-            # when the vector is at q3, adding the angle from q0 to q2, which is 3 * pi/2
-            alpha += 3*math.pi/2
-    elif dx < 0:
-        if dy > 0:
+    if dx < 0:
             # when the vector is at q1, adding the angle of q0, which is pi/2
-            alpha += math.pi/2
-        else:
-            # when the vector is at either q2 (when dy < 0)
-            # adding the angle of q0 and q1, which is 2 * pi/2 = pi
-            # or when on video-x, between q1 and q2 (when dy == 0)
-            # alpha should be precisely pi, but previous calculation
-            # should be alpha = abs(math.atan(slope)) == 0, so same
             alpha += math.pi
-    else:   # dx == 0
+    elif dx == 0:   # dx == 0
         if dy > 0:
             # when the vector is on video-y, between q1 and q0
             # alpha should be precisely pi/2
@@ -147,7 +180,7 @@ def goTo(rx, ry, bx, by, dstx, dsty, curr):
     # aka when less than 200 pixels away from destination
     # slow down the speed as the robot approaches the destination
     if math.hypot(x - dstx, y - dsty) < 200:
-        speedLimit = 10 + 40 * math.hypot(x - dstx, y - dsty) / 200
+        speedLimit = 20 + 30 * math.hypot(x - dstx, y - dsty) / 200
     # when more than 200 pixels away from destination
     # move at "full speed"
     else:
@@ -157,11 +190,11 @@ def goTo(rx, ry, bx, by, dstx, dsty, curr):
     #speedLimit = 20
 
     # when less than 20 pixels away form destination (in one of those cell of the final drawing)
-    #if math.hypot(x - dstx, y - dsty) < 20:
-    #    # pick random speed and GO
-    #    v03 = randint(-9,9)
-    #    v12 = randint(-9,9)
-    #    speedLimit = 30
+    #if math.hypot(x - dstx, y - dsty) < threshold:
+        # pick random speed and GO
+    #    v02 = randint(-5,5)
+    #    v13 = randint(-5,5)
+    #    speedLimit = 15
     # when out of the cell, go towards the center of the destination
     #else:
         # this vector is denoted in robot coordinate system
@@ -178,7 +211,7 @@ def goTo(rx, ry, bx, by, dstx, dsty, curr):
 
     v02 = int((dstx-x)*math.cos(alpha)+(dsty-y)*math.sin(alpha))
     v13 = int(-(dstx-x)*math.sin(alpha)+(dsty-y)*math.cos(alpha))
-    print (v02, v13), int(speedLimit), (int(x), int(y)), int(math.hypot(x - dstx, y - dsty))
+    if DEBUG: print (v02, v13), int(speedLimit), (int(dstx-x), int(dsty-y)),(int(dx), int(dy)), int(math.hypot(x - dstx, y - dsty)), 2*alpha/math.pi
     # considering a simple case where the (x,y) to (dstx, dsty) vector is at rq0, then
     # the motor0 should rotate counter-clockwise (-)
     # the motor1 should rotate counter-clockwise (-)
@@ -193,23 +226,26 @@ def goTo(rx, ry, bx, by, dstx, dsty, curr):
     #        velocities[i] -= int(5*(alpha - prevAlpha))
     #prevAlpha = alpha
 
-    # finally, append speedLimit to the end of the velocities list
-    # convert the list to string and then send to the robot
-    # the string format would be "[v0, v1, v2, v3, sl]"
-    clientSocket.send(str(velocities+[int(speedLimit)]))
+    if not BOTLESS:
+        # finally, append speedLimit to the end of the velocities list
+        # convert the list to string and then send to the robot
+        # the string format would be "[v0, v1, v2, v3, sl]"
+        clientSocket.send(str(velocities+[int(speedLimit)]))
 
-    # get feedback message from the server
-    stringFromServer = clientSocket.recv(1024)
-    print "Server: "+stringFromServer
-    print math.hypot(x - dstx, y - dsty),currP
+        # get feedback message from the server
+        stringFromServer = clientSocket.recv(1024)
+        if DEBUG:
+            print "Server: "+stringFromServer
+            print math.hypot(x - dstx, y - dsty),currP
 
     # This part is to handle the route
     # when less than 30 pixels away from destination
     if math.hypot(x - dstx, y - dsty) < threshold:
-        # pause 0.5 seconds for the robot to stop
-        clientSocket.send(str([0,0,0,0,0]))
-        stringFromServer = clientSocket.recv(1024)
-        time.sleep(0.5)
+        if not BOTLESS:
+            # pause 0.5 seconds for the robot to stop
+            clientSocket.send(str([0,0,0,0,0]))
+            stringFromServer = clientSocket.recv(1024)
+            time.sleep(0.1)
 
         # switch destination to the next point in route list
         if curr[0]+1 < len(route):
@@ -257,7 +293,7 @@ while (camera.isOpened()):
         # centroid
         rc = max(rcnts, key=cv2.contourArea)
         ((rx, ry), rradius) = cv2.minEnclosingCircle(rc)
-        if debug:
+        if DEBUG:
             rM = cv2.moments(rc)
             rcenter = (int(rM["m10"] / rM["m00"]), int(rM["m01"] / rM["m00"]))
 
@@ -268,9 +304,10 @@ while (camera.isOpened()):
                 cv2.circle(frame, (int(rx), int(ry)), int(rradius),
                         (0, 255, 255), 2)
                 cv2.circle(frame, rcenter, int(rradius), (0, 0, 255), -1)
+                cv2.putText(frame, '3', rcenter, 0, 0.2, (255,255,255))
         bc = max(bcnts, key=cv2.contourArea)
         ((bx, by), bradius) = cv2.minEnclosingCircle(bc)
-        if debug:
+        if DEBUG:
             bM = cv2.moments(bc)
             bcenter = (int(bM["m10"] / bM["m00"]), int(bM["m01"] / bM["m00"]))
 
@@ -281,18 +318,21 @@ while (camera.isOpened()):
                 cv2.circle(frame, (int(bx), int(by)), int(bradius),
                         (0, 255, 255), 2)
                 cv2.circle(frame, bcenter, int(bradius), (255, 0, 0), -1)
+                cv2.putText(frame, '1', bcenter, 0, 0.2, (255,255,255))
 
     # go to the dstx and dstb
     #goTo(rx, ry, bx, by, route[currP[0]][0], route[currP[0]][1], currP)
     goTo(rx, ry, bx, by, route[currP[0]][0]+sx, route[currP[0]][1]+sy, currP)
 
     # show the frame to our screen
-    cv2.imshow("Frame", frame)
-    out.write(frame) # write the video to a file
-    #cv2.imshow("Mask", mask)
-    #cv2.imwrite( "frame.jpg", frame); 
-    #cv2.imwrite( "rmask.jpg", rmask); 
-    #cv2.imwrite( "bmask.jpg", bmask); 
+    if PREVIEW:
+        #cv2.imshow("Red Mask", rmask)
+        #cv2.imshow("Blue Mask", bmask)
+        cv2.imshow("Frame", frame)
+        #cv2.imwrite( "frame.jpg", frame); 
+        #cv2.imwrite( "rmask.jpg", rmask); 
+        #cv2.imwrite( "bmask.jpg", bmask); 
+    if OUT: out.write(frame) # write the video to a file
     key = cv2.waitKey(1) & 0xFF
 
     # if the 'q' key is pressed, stop the loop
@@ -300,7 +340,7 @@ while (camera.isOpened()):
         break
 
 # closing stuff before exiting
-clientSocket.close()
+if not BOTLESS: clientSocket.close()
 camera.release()
-out.release()
+if OUT: out.release()
 cv2.destroyAllWindows()
