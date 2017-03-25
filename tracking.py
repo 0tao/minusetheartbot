35f5da69e49c81e7a76c00d08c071c8a68c4fd2d
Jack B. Du <jd2804@nyu.edu>
2017-03-25 21:05:10 +0800

    simulation/BOTLESS added

--- tracking.py
+++ tracking.py
@@ -289,61 +289,74 @@ while (camera.isOpened()):
     # resize the frame, blur it, and convert it to the HSV
     # color space
     frame = imutils.resize(frame, width=width)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color "red" and "blue", then
-    # perform a series of dilations and erosions to remove any
-    # small blobs left in the mask
-    rmask = cv2.inRange(hsv, rlower, rupper)
-    rmask = cv2.erode(rmask, None, iterations=2)
-    rmask = cv2.dilate(rmask, None, iterations=2)
-    bmask = cv2.inRange(hsv, blower, bupper)
-    bmask = cv2.erode(bmask, None, iterations=2)
-    bmask = cv2.dilate(bmask, None, iterations=2)
-    
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    rcnts = cv2.findContours(rmask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-    rcenter = None
-    bcnts = cv2.findContours(bmask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-    bcenter = None
+    if BOTLESS:
 
-    # only proceed if at least one contour was for both red and blue
-    if len(rcnts) > 0 and len(bcnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        rc = max(rcnts, key=cv2.contourArea)
-        ((rx, ry), rradius) = cv2.minEnclosingCircle(rc)
-        if DEBUG:
-            rM = cv2.moments(rc)
-            rcenter = (int(rM["m10"] / rM["m00"]), int(rM["m01"] / rM["m00"]))
+        cv2.circle(frame, (int(rx), int(ry)), int(rradius), (0, 255, 255), 2)
+        cv2.circle(frame, rcenter, int(rradius), (0, 0, 255), -1)
+        cv2.putText(frame, '1', rcenter, 0, 0.2, (255,255,255))
 
-            # only proceed if the radius meets a minimum size
-            if rradius > 0 and rradius < 50:
-                #print rradius, rx, ry
-                # draw the circle and centroid on the frame,
-                cv2.circle(frame, (int(rx), int(ry)), int(rradius),
-                        (0, 255, 255), 2)
-                cv2.circle(frame, rcenter, int(rradius), (0, 0, 255), -1)
-                cv2.putText(frame, '3', rcenter, 0, 0.2, (255,255,255))
-        bc = max(bcnts, key=cv2.contourArea)
-        ((bx, by), bradius) = cv2.minEnclosingCircle(bc)
-        if DEBUG:
-            bM = cv2.moments(bc)
-            bcenter = (int(bM["m10"] / bM["m00"]), int(bM["m01"] / bM["m00"]))
+        cv2.circle(frame, (int(bx), int(by)), int(bradius), (0, 255, 255), 2)
+        cv2.circle(frame, bcenter, int(bradius), (255, 0, 0), -1)
+        cv2.putText(frame, '1', bcenter, 0, 0.2, (255,255,255))
 
-            # only proceed if the radius meets a minimum size
-            if bradius > 0 and bradius < 50:
-                #print bradius, bx, by
-                # draw the circle and centroid on the frame,
-                cv2.circle(frame, (int(bx), int(by)), int(bradius),
-                        (0, 255, 255), 2)
-                cv2.circle(frame, bcenter, int(bradius), (255, 0, 0), -1)
-                cv2.putText(frame, '1', bcenter, 0, 0.2, (255,255,255))
+    else:
+
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        # construct a mask for the color "red" and "blue", then
+        # perform a series of dilations and erosions to remove any
+        # small blobs left in the mask
+        rmask = cv2.inRange(hsv, rlower, rupper)
+        rmask = cv2.erode(rmask, None, iterations=2)
+        rmask = cv2.dilate(rmask, None, iterations=2)
+        bmask = cv2.inRange(hsv, blower, bupper)
+        bmask = cv2.erode(bmask, None, iterations=2)
+        bmask = cv2.dilate(bmask, None, iterations=2)
+        
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        rcnts = cv2.findContours(rmask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+        rcenter = None
+        bcnts = cv2.findContours(bmask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+        bcenter = None
+
+        # only proceed if at least one contour was for both red and blue
+        if len(rcnts) > 0 and len(bcnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            rc = max(rcnts, key=cv2.contourArea)
+            ((rx, ry), rradius) = cv2.minEnclosingCircle(rc)
+            if DEBUG:
+                rM = cv2.moments(rc)
+                rcenter = (int(rM["m10"] / rM["m00"]), int(rM["m01"] / rM["m00"]))
+
+                # only proceed if the radius meets a minimum size
+                if rradius > 0 and rradius < 50:
+                    #print rradius, rx, ry
+                    # draw the circle and centroid on the frame,
+                    cv2.circle(frame, (int(rx), int(ry)), int(rradius),
+                            (0, 255, 255), 2)
+                    cv2.circle(frame, rcenter, int(rradius), (0, 0, 255), -1)
+                    cv2.putText(frame, '3', rcenter, 0, 0.2, (255,255,255))
+            bc = max(bcnts, key=cv2.contourArea)
+            ((bx, by), bradius) = cv2.minEnclosingCircle(bc)
+            if DEBUG:
+                bM = cv2.moments(bc)
+                bcenter = (int(bM["m10"] / bM["m00"]), int(bM["m01"] / bM["m00"]))
+
+                # only proceed if the radius meets a minimum size
+                if bradius > 0 and bradius < 50:
+                    #print bradius, bx, by
+                    # draw the circle and centroid on the frame,
+                    cv2.circle(frame, (int(bx), int(by)), int(bradius),
+                            (0, 255, 255), 2)
+                    cv2.circle(frame, bcenter, int(bradius), (255, 0, 0), -1)
+                    cv2.putText(frame, '1', bcenter, 0, 0.2, (255,255,255))
 
     # go to the dstx and dstb
     #goTo(rx, ry, bx, by, route[currP[0]][0], route[currP[0]][1], currP)
