953ad9af38ff6091b149a96b0ca729b7868f21d7
JackBDu <jd2804@nyu.edu>
2017-03-30 18:23:36 +0800

    output path fixed when no image is provided

--- client.py
+++ client.py
@@ -41,7 +41,7 @@ IMAGE   = args.image
 MARGIN  = args.margin
 RES     = (args.resolution, args.resolution) # for now, assume it's square
 DEPTH   = args.depth
-OUTPATH = IMAGE+'_output/'
+OUTPATH = IMAGE+'_output/' if IMAGE else 'output'
  
 # colors in BGR for convenience
 BLACK   = (0, 0, 0)
